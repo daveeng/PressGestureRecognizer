30b314d878f042b20aef58ed98c853a279dc4110
David Engelhardt <daengelhardt@gmail.com>
2016-06-21 12:26:32 -0400

    Added back in xcode project, as framework

---
+++ PressGestureRecognizer/PressGestureRecognizer.h
@@ -0,0 +1,19 @@
+//
+//  PressGestureRecognizer.h
+//  PressGestureRecognizer
+//
+//  Created by David Engelhardt on 6/21/16.
+//  Copyright © 2016 Viacom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PressGestureRecognizer.
+FOUNDATION_EXPORT double PressGestureRecognizerVersionNumber;
+
+//! Project version string for PressGestureRecognizer.
+FOUNDATION_EXPORT const unsigned char PressGestureRecognizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PressGestureRecognizer/PublicHeader.h>
+
+
